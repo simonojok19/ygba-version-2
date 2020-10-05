c9604f32b7447ac33891ae5f3e411df46d6d1bc5
simonojok19 <simonojok19@gmail.com>
2020-10-05 21:18:38 +0300

    added the module

--- app/src/main/java/org/ygba/youthgobudget/data/community_wishes/CommunityWish.java
+++ app/src/main/java/org/ygba/youthgobudget/data/community_wishes/CommunityWish.java
@@ -1,7 +1,34 @@
 package org.ygba.youthgobudget.data.community_wishes;
 
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 @Entity(tableName = CommunityWishConstants.TABLE_NAME)
 public class CommunityWish {
+    @ColumnInfo(name = CommunityWishConstants.PRIMARY_KEY)
+    @PrimaryKey(autoGenerate = true)
+    private  int primaryKey;
+
+    @ColumnInfo(name = CommunityWishConstants.REGION)
+    private String region;
+
+    @ColumnInfo(name = CommunityWishConstants.DISTRICT)
+    private String district;
+
+    @ColumnInfo(name = CommunityWishConstants.SECTOR)
+    private String sector;
+
+    @ColumnInfo(name = CommunityWishConstants.SUB_COUNTY)
+    private String sub_county;
+
+    @ColumnInfo(name = CommunityWishConstants.FINANCIAL_YEAR)
+    private String finacial_year;
+
+    @ColumnInfo(name = CommunityWishConstants.SERVICE_POINT)
+    private String service_point;
+
+    @ColumnInfo(name = CommunityWishConstants.COMMUNITY_NEEDS)
+    private String community_needs;
+
 }
--- app/src/main/java/org/ygba/youthgobudget/data/community_wishes/CommunityWishConstants.java
+++ app/src/main/java/org/ygba/youthgobudget/data/community_wishes/CommunityWishConstants.java
@@ -2,4 +2,12 @@ package org.ygba.youthgobudget.data.community_wishes;
 
 public class CommunityWishConstants {
     public static final String TABLE_NAME = "table_name_community";
+    public static final String PRIMARY_KEY = "id";
+    public static final String REGION = "region";
+    public static final String DISTRICT = "district";
+    public static final String SECTOR = "sector";
+    public static final String SUB_COUNTY = "sub_county";
+    public static final String FINANCIAL_YEAR = "finacial_year";
+    public static final String SERVICE_POINT = "service_point";
+    public static final String COMMUNITY_NEEDS = "community_needs";
 }
