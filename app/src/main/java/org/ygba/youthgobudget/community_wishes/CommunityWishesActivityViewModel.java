cb022e2b3b4d7185b1169467a7bdee7790213b5c
simonojok19 <simonojok19@gmail.com>
2020-10-05 21:30:53 +0300

    added the view model

---
+++ app/src/main/java/org/ygba/youthgobudget/community_wishes/CommunityWishesActivityViewModel.java
@@ -0,0 +1,12 @@
+package org.ygba.youthgobudget.community_wishes;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class CommunityWishesActivityViewModel extends AndroidViewModel {
+    public CommunityWishesActivityViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
