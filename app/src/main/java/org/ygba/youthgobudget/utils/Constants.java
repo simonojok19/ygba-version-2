c0fad498a88bd076a08c97763b48cc323c1918c7
simonojok19 <simonojok19@gmail.com>
2020-10-06 00:27:56 +0300

    organized files

--- app/src/main/java/org/ygba/youthgobudget/utils/Constants.java
+++ app/src/main/java/org/ygba/youthgobudget/utils/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
     public static final String WATER_COLLECTION_URL = BASE_URL + "collect/water";
     public static final String DISTRICT_COLLECTION_URL = BASE_URL + "areas/district";
     public static final String SUB_COUNTY_COLLECTION_URL = BASE_URL + "areas/sub_counties";
-    public static final String SOCIAL_DEVELOPMENT_COLLECTION_URL = "collect/social-dev";
+    public static final String SOCIAL_DEVELOPMENT_COLLECTION_URL = BASE_URL + "collect/social-dev";
     public static final String COMMUNITY_WISHES_EDUCATION_URL = BASE_URL + "education";
     public static final String COMMUNITY_WISHES_HEALTH_URL = BASE_URL + "health";
     public static final String COMMUNITY_WISHES_WISH_LIST_URL = BASE_URL + "wishlist";
